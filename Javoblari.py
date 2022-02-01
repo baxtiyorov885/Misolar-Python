659ca431235d53d55fca86c721a91bf92876f475
Shaxriyor <baxtiyorov885@gmail.com>
2022-02-01 14:18:45 +0500

    Yangi Misollar Python
    Yangi Misollar Python

---
+++ Javoblari.py
@@ -0,0 +1,45 @@
+"1"
+# a = int(input('kv tomonini kriting>>>'))
+# c = a*4
+# print(f'Tomoni {a} bo\'lgan kvni perimetri {c} ga teng')
+"2"
+# a = int(input("Kvadratning tomonini kriting>>>"))
+# print("Kvadratning yuzi",a*a,"teng")
+"3"
+# a = int(input("Tog\'ri to\'rtburchakning tomonini kriting>>>"))
+# b = int(input("Tog\'ri to\'rtburchakning ikkinchi tomonini kriting>>>"))
+# print('Perimetr =',(a+b)*2,'teng')
+# print('Yuzi =',a*b,'teng')
+"4"
+# a = float(input("Diametrni kriting>>>"))
+# b = 3.14
+# print('Aylananing uzunligi',a*b,"teng")
+"5"
+# a = int(input('Kubning yon tomoning kriting>>>'))
+# print('Kubning hajmi',a*a*a,'teng')
+# print('Kubning sirti',a*6,'teng')
+"6"
+# a = float(input('a ni kriting>>>'))
+# b = float(input('b ni kriting>>>'))
+# c = float(input('c ni kriting>>>'))
+# print('Paralelepedning hajimi',a*b*c,'teng')
+# print('Paralelepedning sirti',2*(a*b+a*c+b*c),'teng')
+"7"
+# l = float(input('Radiusini kriting>>>'))
+# print('Uzunligi',2* 3.14*l,'teng')
+# print('Yuzasi',3.14*l**2,'teng')
+"8"
+# a = float(input('A ni kriting>>>'))
+# b = float(input('B ni kriting>>'))
+# print('O\'rta Arifmetigi',(a+b)/2,'teng')
+"10"
+# a = float(input('A ni kriting>>>'))
+# b = float(input('B ni kriting'))
+# print('a+b=',a+b,'teng')
+# print('a-b',a-b,'teng')
+# print('a*a',a*a,'teng')
+# print('b*b',b*b,'teng')
+# print('a/b',a/b,'teng')
+# print('b/a',b/a,'teng')
+# print('a*b',a*b,'teng')
+
